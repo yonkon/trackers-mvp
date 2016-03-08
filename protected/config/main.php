d99853a489e5a5be35e85da0a9d7cb997df920b8
X-iLeR <deadlove1993@gmail.com>
2016-03-08 04:36:16 +0200

    content only layout 4 site/index copy /assets in2 /js

--- protected/config/main.php
+++ protected/config/main.php
@@ -42,10 +42,13 @@ return array(
 
 		'urlManager'=>array(
 			'urlFormat'=>'path',
+      'showScriptName'=>false,
+      'caseSensitive'=>false,
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+				'<action:\w+>'=>'site/<action>',
 			),
 		),
 
---
+++ protected/views/layouts/content_only.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * @var $this Controller
+ */
+
+echo $content;
