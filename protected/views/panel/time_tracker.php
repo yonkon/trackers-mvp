30471629bb78249c06d5faafab5995f120af4326
X-iLeR <deadlove1993@gmail.com>
2016-04-10 02:37:23 +0300

    time tracker restyle & markup

--- protected/views/panel/time_tracker.php
+++ protected/views/panel/time_tracker.php
@@ -14,8 +14,45 @@
 </div>
 <div class="projects">
   <ol class="pr-list">
-    <li class="project">p1</li>
-    <li class="project">p2</li>
-    <li class="project">addP</li>
+    <li class="project header">
+      <div class="time">today</div>
+      <div class="time">week</div>
+      <div class="time">month</div>
+      <div class="time">custom</div>
+    </li>
+    <? foreach($projects as $project) : ?>
+    <li class="project" data-id="{id}" data-status="{status}">
+      <div class="position">{position}</div>
+      <div class="name">{name}
+        <div class="hid-control">
+          <input>
+          <i class="ok glyphicon glyphicon-ok"></i>
+          <i class="cancel glyphicon glyphicon-remove"></i>
+        </div>
+        <i class="edit right glyphicon glyphicon-pencil"></i>
+      </div>
+      <div class="controls">
+        <? if(false) { ?>
+        <i class="glyphicon glyphicon-time"></i>
+        <i class="glyphicon glyphicon-play-circle"></i>
+        <? } ?>
+        <i class="start green-text glyphicon glyphicon-play"></i>
+        <i class="stop red-text glyphicon glyphicon-stop"></i>
+      </div>
+      <div class="time today">
+        {today}
+      </div>
+      <div class="time week">
+        {week}
+      </div>
+      <div class="time month">
+        {month}
+      </div>
+      <div class="time custom">
+        &nbsp;
+      </div>
+    </li>
+    <? endforeach; ?>
   </ol>
+  <div class="project-add"><i class="glyphicon glyphicon-plus-sign"></i> Добавить</div>
 </div>
